78a9b9e652d57de07cf564b5ff6a3ff84fc65a0f
dga <dima737.99@mail.ru>
2025-06-19 18:48:59 +0300

    maybi fix?

--- src/worker/ldconnector/ld-command.ts
+++ src/worker/ldconnector/ld-command.ts
@@ -73,6 +73,9 @@ export const LD = {
     return ret;
   },
   adb: (name: string, command: string) => {
+    command = command.replace(/\\/g, "\\\\");
+    // command = command.replace(/\//g, "\/\/");
+
     return $`${config.ldPath}\\ldconsole.exe adb --name ${name} --command "${command}"`.text();
   },
   screencap: async (name: string) => {
