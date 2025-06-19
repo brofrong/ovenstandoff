8bd3f080fb1cb14d7e824980148d227c470f6ad3
dga <dima737.99@mail.ru>
2025-06-19 19:20:38 +0300

    WIP

--- src/master-server/guard.ts
+++ src/master-server/guard.ts
@@ -3,6 +3,7 @@ import { env } from "./env";
 export function guard(req: Request): boolean {
   const Authorization = req.headers.get("Authorization");
   const AuthorizationRequest = (new URL(req.url)).searchParams.get('auth');
+  console.log(`Authorization: ${Authorization}, AuthorizationRequest: ${AuthorizationRequest}`);
   if (!Authorization && !AuthorizationRequest) {
     return false;
   }
