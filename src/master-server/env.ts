e18858e13b9528ef393c0ff9b596e2fa84fe3eff
dga <dima737.99@mail.ru>
2025-08-15 14:25:59 +0300

    Update environment configuration: remove CH_SERVER_HOST, change MOCK to a number, and adjust mock runner creation logic to support multiple instances based on MOCK value.

--- src/master-server/env.ts
+++ src/master-server/env.ts
@@ -1,13 +1,12 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  CH_SERVER_HOST: z.string(),
   CH_TOKEN: z.string(),
   SECRET: z.string(),
   PORT: z.coerce.number().default(3000),
   EMAILS: z.string().transform((val) => val.split(',')),
   PASSWORDS: z.string().transform((val) => val.split(',')),
-  MOCK: z.coerce.boolean().default(false),
+  MOCK: z.coerce.number().default(0),
 });
 
 export const env = envSchema.parse(process.env);
