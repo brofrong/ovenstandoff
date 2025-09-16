5b87647f7876a2e5e7eac2b2dace2c0502793c85
dga <dima737.99@mail.ru>
2025-09-16 14:50:28 +0300

    fix

--- packages/worker/script/speedcheck.ts
+++ packages/worker/script/speedcheck.ts
@@ -1,21 +1,23 @@
+import { getConfig } from '@ovenstandoff/shared';
 import { $ } from 'bun';
 import sharp from "sharp";
-import { config } from '../src/config';
-import { LDPlayer } from "../src/ldconnector/ld";
-import { initStorage } from '../src/storage/init-storage';
+import { getLd } from '../../shared/src/ld-command';
+import { LDPlayer } from '../src/ldconnector/ld';
 
 const LD_PLAYER_NAME = 'imt-1';
 const TIME_TO_CHECK = 20;
 
 const checkImgApproach = async () => {
-    const ldPlayer = await new LDPlayer(LD_PLAYER_NAME);
+    const config = await getConfig();
+    const LD = getLd(config);
+    const ldPlayer = new LDPlayer(LD_PLAYER_NAME, LD);
 
     const startTime = Date.now();
 
     for (let i = 0; i < TIME_TO_CHECK; i++) {
         const singleFrameTime = Date.now();
         const img = await ldPlayer.screenShot();
-        const test = await sharp(img).raw().toBuffer();
+        const test = await sharp(img!).raw().toBuffer();
         const endTime = Date.now();
         const timeTaken = endTime - singleFrameTime;
         console.log(`single frame taken: ${timeTaken}ms`);
@@ -27,7 +29,10 @@ const checkImgApproach = async () => {
 }
 
 const checkAdbApproach = async () => {
-    const ldPlayer = await new LDPlayer(LD_PLAYER_NAME);
+    const config = await getConfig();
+    const LD = getLd(config);
+    const ldPlayer = new LDPlayer(LD_PLAYER_NAME, LD);
+
     const startTime = Date.now();
     for (let i = 0; i < TIME_TO_CHECK; i++) {
         const singleFrameTime = Date.now();
@@ -39,8 +44,6 @@ const checkAdbApproach = async () => {
     const timeTaken = endTime - startTime;
     console.log(`\n\n\n!!!!!!!\nTime taken: ${timeTaken}ms\n average: ${timeTaken / TIME_TO_CHECK}ms\n!!!!!!!\n\n\n`);
 }
-
-await initStorage();
 
 // await checkImgApproach();
 await checkAdbApproach();
