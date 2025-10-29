2c50c128135515b5c887f3cb721e703db98bde47
dga <dima737.99@mail.ru>
2025-10-29 20:46:04 +0300

    update test img

--- packages/worker/src/img-proccesing/img-proccesing.test.ts
+++ packages/worker/src/img-proccesing/img-proccesing.test.ts
@@ -1,57 +1,58 @@
 import { expect, test } from "bun:test";
-import { findAnchor } from "./img-proccesing";
+import { findAnchor, findAnchorV2 } from "./img-proccesing";
 import type { AnchorKey } from "./img.type";
+import { anchors } from "../anchors";
 
 test("Find start game", async () => {
-  const ret = await findAnchor("./test-img/imt-1745439699229.png", "play");
+  const ret = await findAnchorV2("./test-img/1761565793413.png", anchors.mainMenuPlay);
   expect(ret).toBe(true);
 });
 
-test("Find menu group", async () => {
-  const ret = await findAnchor(
-    "./test-img/imt-1745439699229.png",
-    "menu_group"
-  );
-  expect(ret).toBe(true);
-});
+// test("Find menu group", async () => {
+//   const ret = await findAnchor(
+//     "./test-img/imt-1745439699229.png",
+//     "menu_group"
+//   );
+//   expect(ret).toBe(true);
+// });
 
-test("Find shareRoom in main menu", async () => {
-  const ret = await findAnchor(
-    "./test-img/imt-1745439699229.png",
-    "share_room_code"
-  );
-  expect(ret).toBe(false);
-});
+// test("Find shareRoom in main menu", async () => {
+//   const ret = await findAnchor(
+//     "./test-img/imt-1745439699229.png",
+//     "share_room_code"
+//   );
+//   expect(ret).toBe(false);
+// });
 
-test("timing", async () => {
-  //preload img;
-  await findAnchor("./test-img/imt-1745439699229.png", "share_room_code");
-  const anchors = [
-    "share_room_code",
-    "competitive_mode",
-    "apply_setting",
-    "lobby_rounds_count_setting",
-    "menu_group",
-  ] as const;
+// test("timing", async () => {
+//   //preload img;
+//   await findAnchor("./test-img/imt-1745439699229.png", "share_room_code");
+//   const anchors = [
+//     "share_room_code",
+//     "competitive_mode",
+//     "apply_setting",
+//     "lobby_rounds_count_setting",
+//     "menu_group",
+//   ] as const;
 
-  const anchorsToProcess: AnchorKey[] = [];
+//   const anchorsToProcess: AnchorKey[] = [];
 
-  for (let i = 0; i < 10; i++) {
-    for (const anchor of anchors) {
-      anchorsToProcess.push(anchor);
-    }
-  }
+//   for (let i = 0; i < 10; i++) {
+//     for (const anchor of anchors) {
+//       anchorsToProcess.push(anchor);
+//     }
+//   }
 
-  const start = Date.now();
+//   const start = Date.now();
 
-  await Promise.all(
-    anchorsToProcess.map((anchor) =>
-      findAnchor("./test-img/imt-1745439699229.png", anchor)
-    )
-  );
+//   await Promise.all(
+//     anchorsToProcess.map((anchor) =>
+//       findAnchor("./test-img/imt-1745439699229.png", anchor)
+//     )
+//   );
 
-  const end = Date.now();
-  const timeToCalculate = end - start;
-  console.log({ timeToCalculate });
-  expect(timeToCalculate).toBeLessThan(200);
-});
+//   const end = Date.now();
+//   const timeToCalculate = end - start;
+//   console.log({ timeToCalculate });
+//   expect(timeToCalculate).toBeLessThan(200);
+// });
