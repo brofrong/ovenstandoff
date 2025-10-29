1c024352b9202159041b4e893a8cad11ffbc5018
Brofrong <brofrong@gmail.com>
2025-10-29 21:10:16 +0300

    fix

--- packages/worker/script/takesceenshot.ts
+++ packages/worker/script/takesceenshot.ts
@@ -4,7 +4,7 @@ import sharp from "sharp";
 import { getLd } from '../../shared/src/ld-command';
 import { LDPlayer } from '../src/ldconnector/ld';
 
-const LD_PLAYER_NAME = 'CH auto 6';
+const LD_PLAYER_NAME = 'CH auto 9';
 
 const config = await getConfig();
 const LD = getLd(config);
